766825d9e6bd98d76def9bf14c979db1e3d058c7
shashank-igp <shashank.sharma@igp.com>
2018-01-08 16:27:50 +0530

    structural changes according to admin panel

---
+++ api/src/main/java/com/igp/handles/admin/mappers/testclass.java
@@ -0,0 +1,8 @@
+package com.igp.handles.admin.mappers;
+
+/**
+ * Created by shanky on 8/1/18.
+ */
+public class testclass
+{
+}
---
+++ api/src/main/java/com/igp/handles/admin/utils/testclasss.java
@@ -0,0 +1,8 @@
+package com.igp.handles.admin.utils;
+
+/**
+ * Created by shanky on 8/1/18.
+ */
+public class testclasss
+{
+}
