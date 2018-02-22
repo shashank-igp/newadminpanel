7e6f1119db446ef4c17c1d17f6ead1d8b1edeca5
igp-tester <testing2@indiangiftsportal.com>
2018-02-22 11:51:50 +0530

    vendor list added in marketplace

--- api/src/main/java/com/igp/admin/endpoints/marketPlace/MarketPlaceOrder.java
+++ api/src/main/java/com/igp/admin/endpoints/marketPlace/MarketPlaceOrder.java
@@ -1,8 +1,8 @@
 package com.igp.admin.endpoints.marketPlace;
 
 import com.igp.admin.mappers.marketPlace.MarketPlaceMapper;
+import com.igp.admin.models.marketPlace.ErrorModel;
 import com.igp.admin.models.marketPlace.MarketPlaceFinalOrderResponseModel;
-import com.igp.admin.models.marketPlace.MarketPlaceResponseModel;
 import com.igp.admin.models.marketPlace.ValidationModel;
 import com.igp.admin.response.EntityFoundResponse;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
@@ -13,6 +13,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -27,18 +28,35 @@ public class MarketPlaceOrder {
     @POST
     @Path("/v1/admin/marketplaceorder")
     public Response performCheckOnCorporateOrder(@QueryParam("user") String user,
-                                                 @DefaultValue("5") @QueryParam("fkasid") Integer fkAssociateId,
+                                                 @QueryParam("value") String userValue,
+                                                 @QueryParam("fkAssociateId") @DefaultValue("0") int loginId,
                                                  final FormDataMultiPart multiPart) throws ParseException {
         MarketPlaceFinalOrderResponseModel marketPlaceFinalOrderResponseModel = new MarketPlaceFinalOrderResponseModel();
         Response response = null;
         MarketPlaceMapper  marketPlaceMapper = new MarketPlaceMapper();
         Map<Integer, Map<String, String>> data = null;
-        // parse the excel file.
-        data = marketPlaceMapper.parseExcelForMarketPlace(multiPart,user+fkAssociateId);
-        // fill the models.
-        List<ValidationModel> validationModelList = marketPlaceMapper.refineDataAndPopulateModels(data,user,fkAssociateId);
-        // validate all the models and create order.
-        marketPlaceFinalOrderResponseModel = marketPlaceMapper.validateDataForMarketPlace(validationModelList);
+        ErrorModel errorModel = new ErrorModel();
+        List<ErrorModel> errorModelList = new ArrayList<>();
+        try{
+            int fkAssociateId = marketPlaceMapper.findVendor(userValue);
+            if(fkAssociateId!=0 && loginId == 1) {
+                // parse the excel file.
+                data = marketPlaceMapper.parseExcelForMarketPlace(multiPart, user + fkAssociateId);
+                // fill the models.
+                List<ValidationModel> validationModelList = marketPlaceMapper.refineDataAndPopulateModels(data, userValue, fkAssociateId);
+                // validate all the models and create order.
+                marketPlaceFinalOrderResponseModel = marketPlaceMapper.validateDataForMarketPlace(validationModelList);
+            }
+            else {
+                errorModel.setMsg("Wrong Vendor Details.");
+                errorModelList.add(errorModel);
+                marketPlaceFinalOrderResponseModel.setError(errorModelList);
+            }
+        }catch (Exception e){
+            errorModel.setMsg("Something Went Wrong.");
+            errorModelList.add(errorModel);
+            marketPlaceFinalOrderResponseModel.setError(errorModelList);
+        }
         response = EntityFoundResponse.entityFoundResponseBuilder(marketPlaceFinalOrderResponseModel);
         return response;
     }
