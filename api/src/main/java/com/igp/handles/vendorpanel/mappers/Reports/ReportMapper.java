4e5a819771a43d44e7cf79a18ff0da464ac5da23
igp-tester <testing2@indiangiftsportal.com>
2018-03-26 15:41:37 +0530

    added new pincode and component in vendor panel

--- api/src/main/java/com/igp/handles/vendorpanel/mappers/Reports/ReportMapper.java
+++ api/src/main/java/com/igp/handles/vendorpanel/mappers/Reports/ReportMapper.java
@@ -4,9 +4,11 @@ package com.igp.handles.vendorpanel.mappers.Reports;
  * Created by shal on 22/9/17.
  */
 
+import com.igp.handles.admin.utils.Reports.ReportUtil;
 import com.igp.handles.vendorpanel.models.Report.*;
 import com.igp.handles.vendorpanel.utils.Order.OrderStatusUpdateUtil;
 import com.igp.handles.vendorpanel.utils.Reports.PayoutAndTaxesReport;
+import com.igp.handles.vendorpanel.utils.Reports.SummaryFunctionsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,24 +50,24 @@ public class ReportMapper {
         boolean result=updateVendorPincode(flag,fk_associate_id,pincode,shipType,updateStatus,updatePrice);
         return result;
     }
-    public static boolean addVendorComponent(String fkAssociateId,String componentCode,String componentName,int type,int price){
-        boolean result=true;
-        String message="Need to add new component name :- "+componentName+" with price :- "+price;
-        //SummaryFunctionsUtil.addVendorComponent(fk_associate_id,componentCode,componentName,type,"dummy.jpg",price);
+    public boolean addVendorComponent(String fkAssociateId,String componentCode,String componentName,int type,int price){
+        SummaryFunctionsUtil summaryFunctionsUtil = new SummaryFunctionsUtil();
+        String message="Need to add new component name :- "+componentCode+" with price :- "+price;
+        boolean result=summaryFunctionsUtil.addVendorComponent(fkAssociateId,componentCode,price);
         OrderStatusUpdateUtil.sendEmailToHandelsTeamToTakeAction(0,fkAssociateId,"",message);
         return result;
     }
 
 
     public PayoutAndTaxReportSummaryModel getPayoutAndTaxes(int fkAssociateId,int orderId,String orderDateFrom,String orderDateTo,String orderDeliveryDateFrom,
-                                                        String orderDeliveryDateTo,String startLimit,String endLimit){
+                                                            String orderDeliveryDateTo,String startLimit,String endLimit){
         PayoutAndTaxesReport payoutAndTaxesReport=new PayoutAndTaxesReport();
         PayoutAndTaxReportSummaryModel payoutAndTaxReportSummaryModel=null;
         String vendorName="";
         try{
             payoutAndTaxReportSummaryModel=payoutAndTaxesReport.getPayoutAndTaxes(
-                                                        fkAssociateId,orderId,orderDateFrom,orderDateTo,orderDeliveryDateFrom,
-                                                        orderDeliveryDateTo,startLimit,endLimit,false,vendorName);
+                fkAssociateId,orderId,orderDateFrom,orderDateTo,orderDeliveryDateFrom,
+                orderDeliveryDateTo,startLimit,endLimit,false,vendorName);
         }catch (Exception exception){
             logger.error("Error in getPayoutAndTaxes ",exception);
         }
@@ -83,14 +85,17 @@ public class ReportMapper {
         return vendorInvoiceModel;
     }
 
-    public static boolean addVendorPincode(String fkAssociateId,int pincode,int cityId,int shipType,int shipCharge){
+    public boolean addVendorPincode(String fkAssociateId,int pincode,int cityId,int shipType,int shipCharge){
+        ReportUtil reportUtil = new ReportUtil();
+        if(shipType==4){
+            shipType=1; // all fixed date are standard
+        }
         Map<Integer,String> map= new HashMap<>();
         map.put(1,"Standard Delivery");
         map.put(2,"Fixed Time Delivery");
         map.put(3,"Mid Night Delivery");
-        map.put(4,"Same Day Delivery");
-        boolean result=true;
         String message="Need to add new pincode :- "+pincode+" with shipping type :- "+map.get(shipType)+" and shipping charge :- "+shipCharge;
+        boolean result = reportUtil.addNewVendorPincodeUtil(new Integer(fkAssociateId).intValue(),pincode,cityId,shipType,shipCharge,0);
         OrderStatusUpdateUtil.sendEmailToHandelsTeamToTakeAction(0,fkAssociateId,"",message);
         return result;
     }
