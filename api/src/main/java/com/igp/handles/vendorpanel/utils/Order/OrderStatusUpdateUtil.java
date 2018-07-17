01e472a729eb6481cf176c08d9f102f804ce2d29
shashank-igp <shashank.sharma@igp.com>
2018-07-17 12:10:08 +0530

    add update dispatch date and deliveryDate while OutForDelivery and Delivered Status update

--- api/src/main/java/com/igp/handles/vendorpanel/utils/Order/OrderStatusUpdateUtil.java
+++ api/src/main/java/com/igp/handles/vendorpanel/utils/Order/OrderStatusUpdateUtil.java
@@ -1,10 +1,8 @@
 package com.igp.handles.vendorpanel.utils.Order;
-
 import com.igp.config.instance.Database;
 import com.igp.handles.admin.utils.Order.OrderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,18 +10,13 @@ import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.text.SimpleDateFormat;
+import java.util.*;
 /**
  * Created by shal on 9/8/17.
  */
 public class OrderStatusUpdateUtil {
-
     private static final Logger logger = LoggerFactory.getLogger(OrderStatusUpdateUtil.class);
-
     public static Boolean updateStatusForOrderUtil(int orderId,String fkAssociateId,String status,String orderProductIds,
         String recipientInfo,String rejectionMessage,String commentsDelivered,String recipientName,int rejectionType
         ,String ipAddress,String userAgent){
@@ -53,7 +46,6 @@ public class OrderStatusUpdateUtil {
                         result=false;
                     }
                     break;
-
                 case "OutForDelivery":
                     result=markOutForDelivery(orderId,fkAssociateId,"Shipped",orderProductIds);
                     if (result) {
@@ -70,7 +62,6 @@ public class OrderStatusUpdateUtil {
                     else {
                         result=false;
                     }
-
                     break;
                 case "Delivered":
                     deliveryAttemptFlag=0;
@@ -142,10 +133,8 @@ public class OrderStatusUpdateUtil {
         catch (Exception exception){
             logger.error("Exception while updating", exception);
         }
-
         return result;
     }
-
     public  static Boolean  markConfirmedOrdersProducts(int orderId, String fkAssociateId, String status, String orderProductIds){
         Connection connection = null;
         String statement;
@@ -171,7 +160,6 @@ public class OrderStatusUpdateUtil {
             else {
                 ifUpdationSucessfull=false;
             }
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
@@ -181,21 +169,27 @@ public class OrderStatusUpdateUtil {
         }
         return ifUpdationSucessfull;
     }
-
-
     public static Boolean   markOutForDelivery(int orderId, String fkAssociateId, String status, String orderProductIds){
         Connection connection = null;
         String statement;
         Boolean ifUpdationSucessfull=false;
         PreparedStatement preparedStatement = null;
         try{
+            String pattern = "yyyy-MM-dd";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            String date = simpleDateFormat.format(new Date());
             connection = Database.INSTANCE.getReadWriteConnection();
-            statement = "update orders_products as op inner join trackorders as tk on op.orders_products_id=tk.orders_products_id and  op.orders_id=tk.orders_id  set tk.outForDeliveryDate=now() ,op.orders_product_status= ? , op.delivery_status = ? where  op.orders_id=? and op.fk_associate_id in(?) and op.orders_products_id in ( "+orderProductIds+") ";
+            statement = "update orders_products as op inner join trackorders as tk on " +
+                "op.orders_products_id=tk.orders_products_id and  op.orders_id=tk.orders_id " +
+                " set tk.outForDeliveryDate=now() ,op.orders_product_status= ?,op.dispatch_date=?, op.delivery_status = ? " +
+                "where  op.orders_id=? and op.fk_associate_id in(?) and op.orders_products_id in" +
+                " ( "+orderProductIds+") ";
             preparedStatement = connection.prepareStatement(statement);
             preparedStatement.setString(1,status);
-            preparedStatement.setInt(2,0);
-            preparedStatement.setInt(3,orderId);
-            preparedStatement.setString(4,fkAssociateId);
+            preparedStatement.setString(2,date);
+            preparedStatement.setInt(3,0);
+            preparedStatement.setInt(4,orderId);
+            preparedStatement.setString(5,fkAssociateId);
             //     preparedStatement.setString(4,orderProductIds);
             logger.debug("sql query "+preparedStatement);
             Integer rowsUpdated = preparedStatement.executeUpdate();
@@ -205,7 +199,6 @@ public class OrderStatusUpdateUtil {
             else {
                 ifUpdationSucessfull=false;
             }
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
@@ -213,18 +206,20 @@ public class OrderStatusUpdateUtil {
             Database.INSTANCE.closeConnection(connection);
         }
         return ifUpdationSucessfull;
-
-
     }
     public static Boolean markDelivered(int orderId, String fkAssociateId, String status, String orderProductIds,int deliveryAttemptFlag){
-
         Connection connection = null;
         String statement;
         Boolean ifUpdationSucessfull=false;
         PreparedStatement preparedStatement = null;
         try{
             connection = Database.INSTANCE.getReadWriteConnection();
-            statement = "update orders_products as op inner join trackorders as tk on op.orders_products_id=tk.orders_products_id and  op.orders_id=tk.orders_id  set tk.deliveredDate=now() , op.orders_product_status = ? ,op.delivery_status= 1,op.delivery_attempt = case when op.delivery_attempt = 2 then 3 else  "+ deliveryAttemptFlag +" end where  op.orders_id=? and op.fk_associate_id in(?) and op.orders_products_id in ( "+orderProductIds+") ";
+            statement = "update orders_products as op inner join trackorders as tk on " +
+                "op.orders_products_id=tk.orders_products_id and  op.orders_id=tk.orders_id  " +
+                "set tk.deliveryDate = now(),tk.deliveredDate=now() , op.orders_product_status = ? ,op.delivery_status= 1," +
+                "op.delivery_attempt = case when op.delivery_attempt = 2 then 3 else  "+ deliveryAttemptFlag +"" +
+                " end where  op.orders_id=? and op.fk_associate_id in(?) and op.orders_products_id in " +
+                "( "+orderProductIds+") ";
             preparedStatement = connection.prepareStatement(statement);
             preparedStatement.setString(1,"Shipped");
             preparedStatement.setInt(2,orderId);
@@ -233,12 +228,10 @@ public class OrderStatusUpdateUtil {
             Integer rowsUpdated = preparedStatement.executeUpdate();
             if (rowsUpdated>0){
                 ifUpdationSucessfull=true;
-
             }
             else {
                 ifUpdationSucessfull=false;
             }
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
@@ -247,9 +240,7 @@ public class OrderStatusUpdateUtil {
         }
         return ifUpdationSucessfull;
     }
-
     public static void updateOrderToStatusOrPartiallyStatus(int orderId, String orderStatus){
-
         Connection connection = null;
         String statement;
         ResultSet resultSet = null;
@@ -273,7 +264,6 @@ public class OrderStatusUpdateUtil {
             resultSet=preparedStatement.executeQuery();
             while (resultSet.next()){
                 value=statusValue.get(resultSet.getString("op.orders_product_status"));
-
                 if(value==1)
                     a=1;
                     // marking if status is processed
@@ -283,13 +273,10 @@ public class OrderStatusUpdateUtil {
                 else if(value==3)
                     c=1;
                 // marking if status is shipped
-
                 if (max<value)
                     max=value;
                 // checking which status has more priority
                 // (e.g. one is shipped other is confirmed then we mark according to shipped)
-
-
             }
             if((a+b+c)==1) {
                 // checking if all the status are same
@@ -299,7 +286,6 @@ public class OrderStatusUpdateUtil {
                 else if(max==2)
                     updateOrderStatus("Confirmed", orderId);
             }
-
             else {
                 // checking if the status of different products in an order mismatch
                 if (max==3)
@@ -308,7 +294,6 @@ public class OrderStatusUpdateUtil {
                 else if(max==2)
                     updateOrderStatus("Partially Confirmed", orderId);
             }
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
@@ -316,14 +301,8 @@ public class OrderStatusUpdateUtil {
             Database.INSTANCE.closeResultSet(resultSet);
             Database.INSTANCE.closeConnection(connection);
         }
-
-
-
     }
-
-
     public static Boolean markRejected(int orderId, String fkAssociateId, String status, String orderProductIds){
-
         Connection connection = null;
         String statement;
         Boolean ifUpdationSucessfull=false;
@@ -344,7 +323,6 @@ public class OrderStatusUpdateUtil {
             else {
                 ifUpdationSucessfull=false;
             }
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
@@ -353,9 +331,7 @@ public class OrderStatusUpdateUtil {
         }
         return ifUpdationSucessfull;
     }
-
     public  static  void updateOrderStatus(String status,int orderId){
-
         Connection connection = null;
         String statement;
         PreparedStatement preparedStatement = null;
@@ -367,22 +343,17 @@ public class OrderStatusUpdateUtil {
             preparedStatement.setInt(2,orderId);
             logger.debug("sql query "+preparedStatement);
             int rows=preparedStatement.executeUpdate();
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
             Database.INSTANCE.closeStatement(preparedStatement);
             Database.INSTANCE.closeConnection(connection);
         }
-
-
-
     }
     public  static  void updateHandelsHistory(int orderId, String info, String message, String recipientInfo,
         String rejectionMessage, String status, String orderProductIds,
         String commentsDelivered,String recipientName,int rejectionType,
         String fkAssociateId ){
-
         Connection connection = null;
         String statement;
         PreparedStatement preparedStatement = null;
@@ -394,15 +365,11 @@ public class OrderStatusUpdateUtil {
         rejectionTypeToReasonMap.put(5,"Fixed time or Midnight delivery not possible");
         rejectionTypeToReasonMap.put(6,"Duplicate order");
         rejectionTypeToReasonMap.put(7,"Other");
-
-
         try{
-
             List<String> orderProductsLists = Arrays.asList(orderProductIds.split(","));
             StringBuilder sb=new StringBuilder("VALUES");
             for (int i = 0; i < orderProductsLists.size(); i++) {
                 if (status.equals("Rejected")){
-
                     if (i==orderProductsLists.size()-1){
                         sb.append("(" + orderProductsLists.get(i) + "," + orderId + "," + rejectionType + ",'" + rejectionMessage + "',now())");
                     }
@@ -425,7 +392,6 @@ public class OrderStatusUpdateUtil {
             }
             else { // i.e Rejected
                 statement="INSERT INTO `handels_history` (`orders_products_id`,`orders_id`, `rejection_type` , `rejection_message`, `insertTime`) "+sb.toString()+" ON DUPLICATE KEY UPDATE orders_id="+orderId+",rejection_type="+rejectionType+",rejection_message='"+rejectionMessage+"' ,insertTime= now();";
-
                 sendEmailToHandelsTeamToTakeAction(orderId,fkAssociateId,rejectionTypeToReasonMap.get(rejectionType),rejectionMessage);
             }
             preparedStatement = connection.prepareStatement(statement);
@@ -437,10 +403,7 @@ public class OrderStatusUpdateUtil {
             Database.INSTANCE.closeStatement(preparedStatement);
             Database.INSTANCE.closeConnection(connection);
         }
-
-
     }
-
     public static void updateOrderHistory(String stringToUpdate, int orderId, String ordersProductsIds, String fkAssociateId){
         Connection connection = null;
         String statement;
@@ -454,21 +417,14 @@ public class OrderStatusUpdateUtil {
             preparedStatement.setString(3,fkAssociateId);
             logger.debug("sql query "+preparedStatement);
             Integer rowsUpdated = preparedStatement.executeUpdate();
-
-
         } catch (Exception exception) {
             logger.error("Exception in connection", exception);
         } finally {
             Database.INSTANCE.closeStatement(preparedStatement);
             Database.INSTANCE.closeConnection(connection);
         }
-
     }
-
-
-
     public static void sendEmailForOrder(int orderId,String status,String fkAssociateId,String productIds){
-
         try
         {
             URL url = new URL("http://admin.indiangiftsportal.com/myshop/sendEmail.php");
@@ -476,29 +432,22 @@ public class OrderStatusUpdateUtil {
             String postData = null;
             // CURLOPT_POST
             con.setRequestMethod("POST");
-
             // CURLOPT_FOLLOWLOCATION
             con.setInstanceFollowRedirects(true);
             if (status.equals("OutForDelivery")){
                 postData = "action=Dispatched&orderid="+orderId+"&associd="+fkAssociateId+"&productIds="+productIds;
             }
-
             else if (status.equals("Delivered")){
                 postData = "action=Delivered&orderid="+orderId+"&associd="+fkAssociateId+"&productIds="+productIds;
             }
             // URLEncoder.encode(emailBodyWithActualContents, "UTF-8");
-
-
             //String postData = "to="+recipient+"&sub=ForgotPassword&body=sadasdasdasdasd";
             con.setRequestProperty("Content-length", String.valueOf(postData.length()));
-
             con.setDoOutput(true);
             con.setDoInput(true);
-
             DataOutputStream output = new DataOutputStream(con.getOutputStream());
             output.writeBytes(postData);
             output.close();
-
             // "Post data send ... waiting for reply");
             int code = con.getResponseCode(); // 200 = HTTP_OK
             logger.debug("EmailCURL: "+status+" :: "+orderId+" :: :: Response Code - "+code);
@@ -527,19 +476,13 @@ public class OrderStatusUpdateUtil {
             body+="Regarding :- "+regarding+"\n\n";
             body+="Message from Vendor :-   "+message+"  \n\n";
             postData+="action=HandelVendorPanel&associd="+fkAssociateId+"&subject="+subject+"&body="+ URLEncoder.encode(body,"UTF-8");
-
-
-
             //String postData = "to="+recipient+"&sub=ForgotPassword&body=sadasdasdasdasd";
             con.setRequestProperty("Content-length", String.valueOf(postData.length()));
-
             con.setDoOutput(true);
             con.setDoInput(true);
-
             DataOutputStream output = new DataOutputStream(con.getOutputStream());
             output.writeBytes(postData);
             output.close();
-
             // "Post data send ... waiting for reply");
             int code = con.getResponseCode(); // 200 = HTTP_OK
         }
@@ -576,9 +519,7 @@ public class OrderStatusUpdateUtil {
         }
         return productIds;
     }
-
     //RejectionType >> 0=no rejection,1=Delivery location not serviceable,2=Product not available,3=Capacity full,
     // 4=Customers instruction,5=Delivery not possible on given date,6=Fixed time or Midnight not possible,
     // 7=Duplicate order,8=Other,9=Alternate Number/Address issues
 }
-
